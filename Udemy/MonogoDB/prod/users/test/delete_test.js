52ab05c15d908640ebe4e4b1c6c63bc7741939e1
flcarre <flcarre@student.42.fr>
2019-06-17 09:30:59 +0200

    fastpush

--- Udemy/MonogoDB/prod/users/test/delete_test.js
+++ Udemy/MonogoDB/prod/users/test/delete_test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 describe('Deleting user', () => {
     let joe;
 
-    beforeEach((done), () => {
+    beforeEach((done) => {
         joe = new User({ name: 'Joe'});
         joe.save()
             .then(() => done());
@@ -12,18 +12,42 @@ describe('Deleting user', () => {
 
     it('model instance remove', (done) => {
         joe.remove()
+            .then(() => User.findOne({ name: 'Joe'}))
+            .then((user) => {
+                assert(user === null);
+                done();
+            })
     });
 
-    it('class method remove', () => {
+    it('class method remove', (done) => {
+        // remove un groupe duser suivant des criteres
+        User.remove({ name: 'Joe'})
+            .then(() => User.findOne({ name: 'Joe'}))
+            .then((user) => {
+                assert(user === null);
+                done();
+            })
 
     });
 
-    it('class method findAndRemove', () => {
-
+    it('class method findAndRemove', (done) => {
+        // remove la première instance correspondant au critere rentré dans mongodb
+        User.findOneAndRemove({ name: 'Joe' })
+            .then(() => User.findOne({ name: 'Joe'}))
+            .then((user) => {
+                assert(user === null);
+                done();
+            })
     });
 
-    it('class method findByIdAndRemove', () => {
-
+    it('class method findByIdAndRemove', (done) => {
+        // remove la première instance correspondant a l'id rentré dans mongodb
+        User.findByIdAndRemove(joe._id)
+            .then(() => User.findOne({ name: 'Joe'}))
+            .then((user) => {
+                assert(user === null);
+                done();
+            })
     });
 
 });
